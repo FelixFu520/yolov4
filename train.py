edc9c61a7e6ee57ea84a8fb58cd92412e0f88d24
fusimeng <fufaloveguomengkai@163.com>
2020-11-03 14:16:49 +0800

    注释

--- train.py
+++ train.py
@@ -477,7 +477,6 @@ def train(model, device, config, epochs=5, batch_size=1, save_cp=True, log_step=
                                           scheduler.get_lr()[0] * config.batch))
 
                 pbar.update(images.shape[0])
-                break
 
             if cfg.use_darknet_cfg:
                 eval_model = Darknet(cfg.cfgfile, inference=True)
